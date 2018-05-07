8a59ef3fe229b1af9e49452e38cd79c9b5ce368b
Rahadian Yusuf <rahadian.y@gmail.com>
2018-05-07 18:31:04 +0700

    modify test for tick

--- __tests__/tick.test.js
+++ __tests__/tick.test.js
@@ -1,25 +1,35 @@
-// import Miru from '../src';
-
 jest.useFakeTimers();
 
 describe('Tick', () => {
-  test('runs', () => {
+  test('runs interval', () => {
     const Miru = require('../src/index').default;
 
     const vm = new Miru({
       data() {
         return { number: 0 };
-      }
+      },
     });
 
-    Miru.$nextTick(() => {
-      vm.number = 1;
-    });
+    const func = jest.fn();
+
+    Miru.$nextTick(func);
 
     expect(setInterval).toHaveBeenCalledTimes(1);
+  });
 
-    Miru.$nextTick(() => {
-      expect(vm.number).toBe(1);
-    });
-  })
+  test('runs function defined', () => {
+    const Miru = require('../src/index').default;
+
+    const vm = new Miru({});
+
+    const callback = jest.fn();
+
+    Miru.$nextTick(callback);
+    expect(callback).not.toBeCalled();
+
+    jest.runOnlyPendingTimers();
+
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
 });
