5ad8cd18071ff0d87f7cae69b1ff0bef435e5b32
Rahadian Yusuf <rahadian.y@gmail.com>
2018-05-13 11:13:48 +0700

    add event

--- __tests__/h.test.jsx
+++ __tests__/h.test.jsx
@@ -1,18 +1,27 @@
 import h from '../src/utils/h';
 
 describe('h', () => {
-  test('parse simple ', () => {
+  test('parse simple jsx', () => {
     const dom = <div id="test"></div>;
 
     expect(dom.data.props).toBeDefined();
+    expect(dom.data.props.id).toBe('test');
   });
 
-  test('parse simple ', () => {
+  test('parse children and have correct number of children', () => {
     const dom = <div id="test">
       <span>Hello</span>
       <span>Hello</span>
     </div>;
 
     expect(dom.children.length).toBe(2);
+  });
+
+  test('add event listener ', () => {
+    const dom = <button on-click="true" id="test">
+      </button>;
+
+    expect(dom.data.on).toBeDefined();
+    expect(dom.data.on.click).toBeDefined();
   });
 });
