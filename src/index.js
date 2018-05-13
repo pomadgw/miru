b9f168da2f7cc106f31c8b53cf3bddeb76461e10
Rahadian Yusuf <rahadian.y@gmail.com>
2018-05-13 17:52:50 +0700

    Add Virtual DOM and JSX (#13)
    * init add virtual dom
    
    * add dom patching
    
    * init add jsx
    
    * add children
    
    * add event
    
    * use h from utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // @flow
 import nextTick from './utils/tick';
-import { $pInit } from './utils/data';
+import { $p, $pInit } from './utils/data';
+import { mount } from './utils/vdom';
 import { setData, setComputed, setMethod, setWatcher } from './setters';
+
+function noop() {}
 
 export default class Miru {
   constructor({
-    data = {}, methods = {}, computed = {}, watch = {},
+    data = {},
+    methods = {},
+    computed = {},
+    watch = {},
+    render = noop,
   } = {}) {
     $pInit(this);
 
@@ -13,6 +20,12 @@ export default class Miru {
     setMethod(this, methods);
     setComputed(this, computed);
     setWatcher(this, watch);
+
+    $p(this).render = render.bind(this);
+  }
+
+  $mount(selector) {
+    mount(this, selector);
   }
 
   $nextTick(func) {
