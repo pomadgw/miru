54a45ecb563bad12ed6a4103fb66193d5867b520
Rahadian Yusuf <rahadian.y@gmail.com>
2018-05-13 10:30:03 +0700

    add children

--- __tests__/h.test.jsx
+++ __tests__/h.test.jsx
@@ -5,5 +5,14 @@ describe('h', () => {
     const dom = <div id="test"></div>;
 
     expect(dom.data.props).toBeDefined();
-  })
+  });
+
+  test('parse simple ', () => {
+    const dom = <div id="test">
+      <span>Hello</span>
+      <span>Hello</span>
+    </div>;
+
+    expect(dom.children.length).toBe(2);
+  });
 });
