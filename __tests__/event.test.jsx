8007d43e3fb17b45df3249f8e4188eae3bc56d4a
Rahadian Yusuf <rahadian.y@gmail.com>
2018-05-20 13:47:04 +0700

    add test

--- __tests__/event.test.jsx
+++ __tests__/event.test.jsx
@@ -24,4 +24,34 @@ describe('Event', () => {
     expect(vm.number).not.toBe(0);
     expect(vm.number).toBe(1);
   })
+  test('event from jsx works', () => {
+    const vm = new Miru({
+      data() {
+        return {
+          number: 0,
+        };
+      },
+      methods: {
+        update() {
+          this.number += 1;
+        }
+      },
+      render(h) {
+        return (
+          <div>
+            <div id="value">{this.number}</div>
+            <button id="update" on-click={this.update}>+</button>
+          </div>);
+      }
+    });
+
+    document.body.innerHTML = `<div id="app"></div>`;
+
+    vm.$mount('#app');
+
+    document.getElementById('update').click();
+
+    expect(vm.number).not.toBe(0);
+    expect(vm.number).toBe(1);
+  })
 });
