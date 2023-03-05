c58ad2ed97923d908c3bbbd6db02c6a8470a5637
Rahadian Yusuf <rahadian.y@gmail.com>
2023-03-05 18:52:20 +0700

    use snabbdom

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,34 @@
-import { render, type VirtualDOM } from '../lib/vdom'
+import { init, classModule, propsModule, styleModule, eventListenersModule, h, type VNode } from 'snabbdom'
+
+const patch = init([
+  // Init patch function with chosen modules
+  classModule, // makes it easy to toggle classes
+  propsModule, // for setting properties on DOM elements
+  styleModule, // handles styling on elements with support for animations
+  eventListenersModule // attaches event listeners
+])
 
 const root = document.querySelector('#app')
 
-const component = (): VirtualDOM => ({
-  tag: 'main',
-  children: [
-    {
-      tag: 'p',
-      props: {
-        style: 'font-weight: bold'
-      },
-      events: {
-        click: () => {
-          console.log('Long live King Souma!')
+const component = (): VNode =>
+  h('main', [
+    h(
+      'p',
+      {
+        style: { fontWeight: 'bold' },
+        on: {
+          click: () => {
+            console.log('Long live King Souma!')
+          }
         }
       },
-      children: [
-        {
-          tag: null,
-          children: 'United Kingdom of Elfrieden and Amidonia'
-        }
-      ]
-    }
-  ]
-})
+      'United Kingdom of Elfrieden and Amidonia'
+    )
+  ])
 
 function doRender(): void {
   if (root !== null) {
-    root.appendChild(render(component()))
+    patch(root, component())
   }
 }
 
